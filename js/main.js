160ce88494af319f4797b2a886dbfa89a88c0e0a
toth8j <toth.jozsef8@mav-szk.hu>
2017-05-25 16:11:45 +0200

    HTML5_1 feltöltése 2. nap

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 function bigSection() {
     var section1 = document.querySelector("#section1");
-    section1.classList.add("big");
+
+    if ( section1.classList.contains("big") ) {
+        section1.classList.remove("big");
+    } else {
+        section1.classList.add("big");
+    }
+
 }/**
  * Created by toth8j on 2017. 05. 24..
  */
